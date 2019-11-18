15ad97e4f74d66eae22961b5c2ee19113122745b
Matt FowlerFinn <matt.fowlerfinn@gmail.com>
2019-11-18 11:07:41 -0600

    adjusted layout

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # catch-of-the-day
+
 React app with local storage and Firebase database.
+
+Deployed at https://catch-of-the-day.mattfowlerfinn.now.sh/
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,23 +1,37 @@
-import React from 'react'
-import { formatPrice } from '../helpers'
+import React from "react";
+import { formatPrice } from "../helpers";
+import PropTypes from "prop-types";
 
 class Fish extends React.Component {
-	render () {
+	static propTypes = {
+		details: PropTypes.shape({
+			image: PropTypes.string,
+			name: PropTypes.string,
+			price: PropTypes.number,
+			desc: PropTypes.string,
+			status: PropTypes.string
+		}),
+		addToOrder: PropTypes.func
+	}
+	render() {
 		const { image, name, price, desc, status } = this.props.details;
-		const isAvailable = status === 'available';
+		const isAvailable = status === "available";
 		return (
 			<li className="menu-fish">
 				<img src={image} alt={name} />
-				<h3 className="fish-name">{name}
+				<h3 className="fish-name">
+					{name}
 					<span className="price">{formatPrice(price)}</span>
 				</h3>
 				<p>{desc}</p>
-				<button disabled={!isAvailable} onClick={() => this.props.addToOrder(this.props.index)} >
-					{isAvailable ? 'Add To Order' : 'Sold Out!'}
+				<button
+					disabled={!isAvailable}
+					onClick={() => this.props.addToOrder(this.props.index)}
+				>
+					{isAvailable ? "Add To Order" : "Sold Out!"}
 				</button>
-
 			</li>
-		)
+		);
 	}
 }
 
